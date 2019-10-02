88b67c409719c993c37fbef2ee866e3dc5049f8e
Brad Farberman <bfarberman@gmail.com>
2019-10-02 20:24:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(string)
   string_array = string.split " "
-  reverse_string = string_array.collect( |word| word.reverse
+  reverse_string = string_array.collect{|word| word.reverse}
